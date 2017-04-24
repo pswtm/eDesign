02d2c288a575e41bf31ad5d28fa968db5cb4653e
XQF4K <fabiankuschke@Fabians-MacBook-Pro.local>
2017-04-24 22:56:52 +0200

    bitte funktioniere grrr

--- src/Bauelemente/Kondensator.java
+++ src/Bauelemente/Kondensator.java
@@ -3,5 +3,13 @@ package Bauelemente;
 /**
  * Created by Merve on 24.04.2017.
  */
-public class Kondensator implements Bauelement {
+public class Kondensator extends Bauelement {
+
+    public Kondensator(int x, int y, int Orientation)
+    {
+        super(x,y,Orientation);
+
+        System.out.println("Class Kon: "+posX+posY+Orientation);
+    }
+
 }
--- src/Bauelemente/Widerstand.java
+++ src/Bauelemente/Widerstand.java
@@ -3,5 +3,11 @@ package Bauelemente;
 /**
  * Created by Merve on 24.04.2017.
  */
-public class Widerstand implements Bauelement {
+public class Widerstand extends Bauelement {
+
+    public Widerstand(int x, int y, int Orientation)
+    {
+        super(x,y,Orientation);
+        System.out.println("Class Wid: "+posX+posY+Orientation);
+    }
 }
