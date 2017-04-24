4a1c9b705731d9b99e4bc06e7615da0be8b309a2
XQF4K <fabiankuschke@Fabians-MacBook-Pro.local>
2017-04-24 23:13:45 +0200

    los geh

--- src/Bauelemente/Spannungsquelle.java
+++ src/Bauelemente/Spannungsquelle.java
@@ -3,5 +3,13 @@ package Bauelemente;
 /**
  * Created by Merve on 24.04.2017.
  */
-public class Spannungsquelle implements Bauelement {
+public class Spannungsquelle extends Bauelement {
+
+    public Spannungsquelle(int x, int y, int Orientation)
+    {
+        super(x,y,Orientation);
+
+        System.out.println("Class Spann: "+posX+posY+Orientation);
+
+    }
 }
