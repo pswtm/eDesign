c9727a606b0dd77614c25a913427c292e7423c1e
XQF4K <fabiankuschke@Fabians-MacBook-Pro.local>
2017-05-11 21:47:08 +0200

    linien drag versuch weiter gekommen

--- src/Bauelemente/Leitung.java
+++ src/Bauelemente/Leitung.java
@@ -11,6 +11,7 @@ import javafx.scene.shape.Line;
 public class Leitung extends Bauelement{
     double xend;
     double yend;
+    double xs,ys,xe,ye;
     Color color=Color.rgb(238,238,238);
     Line line =new Line();
 
@@ -20,7 +21,7 @@ public class Leitung extends Bauelement{
         super(xstart,ystart,Orientation);
         this.xend=xend;
         this.yend=yend;
-        //System.out.println("Class Leitung: "+xstart+","+ystart+","+xend+","+yend);
+        System.out.println("Class Leitung: "+xstart+","+ystart+","+xend+","+yend);
         line.setStartX(xstart);
         line.setStartY(ystart);
         line.setEndX(xend);
@@ -35,26 +36,59 @@ public class Leitung extends Bauelement{
         {
             //TOdo Berechnuns wo die Linie sein muss
             //Rechnung wo Maus ist und Länge der Linie zum positionieren
-            double xs=xstart-event.getSceneX();
-            double ys=ystart-event.getSceneY();
-            double xe=xend-event.getSceneX();
-            double ye=yend-event.getSceneY();
-            System.out.println("dragged xs:"+xs+" ys: "+ys+" Maus x: "+event.getSceneX()+" Maus y: "+event.getSceneY());
-            line.setStartX(xs);
-            line.setStartY(ys);
-            line.setEndX(xe);
-            line.setEndY(ye);
+            //System.out.println("Line alt xs:"+posX+" ys: "+posY+" xe: "+xend+" ye: "+yend);
+            //xs=posX-event.getSceneX();
+            //ys=posY-event.getSceneY();
+            //xe=event.getSceneX()-xend;
+            //ye=event.getSceneY()-yend;
+            //System.out.println("dragged falsch xs:"+xs+" ys: "+ys+" Maus x: "+event.getSceneX()+" Maus y: "+event.getSceneY());
+            //line.setStartX(xs);
+            //line.setStartY(ys);
+            //line.setEndX(xe);
+            //line.setEndY(ye);
         }});
         //zeichnet wenn Maus losgelassen
         line.setOnMouseReleased(new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent event)
             {
-                System.out.println("dropped");
-                line.setStartX(xstart);
-                line.setStartY(ystart);
-                line.setEndX(xend);
-                line.setEndY(yend);
+                if(posX>event.getSceneX()&&posY>event.getSceneY())
+                {
+                    xs= event.getSceneX()-posX;
+                    ys=event.getSceneY()-posY;
+                    System.out.println("Startpunkt unten rechts. posx: "+posX+ " >Mausx:"+event.getSceneX()+" posy: "+posY+" >Mausy "+event.getSceneY());
+                    System.out.println(xs+","+ys);
+                }
+                else if(posX<event.getSceneX()&&posY<event.getSceneY())
+                {
+                    System.out.println("Startpunkt oben links. posx: "+posX+ " <Mausx:"+event.getSceneX()+" posy: "+posY+" <Mausy "+event.getSceneY());
+                }
+                else if(posX>event.getSceneX()&&posY<event.getSceneY())
+                {
+                    System.out.println("Startpunkt oben rechts. posx: "+posX+ " >Mausx:"+event.getSceneX()+" posy: "+posY+" <Mausy "+event.getSceneY());
+                }
+                else if(posX<event.getSceneX()&&posY>event.getSceneY())
+                {
+                    System.out.println("Startpunkt unten links. posx: "+posX+ " <Mausx:"+event.getSceneX()+" posy: "+posY+" >Mausy "+event.getSceneY());
+                }
+                else {
+                    System.out.println("Startpunkt gleich. posx: "+posX+ " Mausx:"+event.getSceneX()+" posy: "+posY+" Mausy "+event.getSceneY());
+                    return;
+                }
+                //xs=event.getSceneX()-posX;
+                //ys=event.getSceneY()-posY;
+                //xe=xend-event.getSceneX();
+                //ye=yend-event.getSceneY();
+
+                //System.out.println("Line alt xs:"+posX+" ys: "+posY+" xe: "+xend+" ye: "+yend);
+                //System.out.println("dropped falsch xs:"+xs+" ys: "+ys+" xe: "+xe+" ye: "+ye);
+                //System.out.println("Maus x: "+event.getSceneX()+" Maus y: "+event.getSceneY());
+
+                //line.setStartX(xs+event.getSceneX());
+                //line.setStartY(ys+event.getSceneY());
+                //line.setEndX(xe+event.getSceneX());
+                //line.setEndY(ye+event.getSceneY());
+
             }});
     }
     //Wird zum String xml hinzugefügt
